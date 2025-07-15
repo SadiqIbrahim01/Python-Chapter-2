ddd34311bc8ea661a2e35782a6b1237f883a6ca7
SadiqIbrahim01 <164149043+SadiqIbrahim01@users.noreply.github.com>
2025-07-15 10:07:37 -0700

    Add files via upload

---
+++ Chapter Two/2,7.py
@@ -0,0 +1,9 @@
+2.7
+
+print('Enter an integer to check whether it is odd or even')
+
+number = int (input('Enter your integer: '))
+
+if  number % 2 == 0:
+
+print('Your', number 'is an even number')
