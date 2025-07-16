a888fefe9414404fdcc20a9dfbfb28c250794f73
SadiqIbrahim01 <164149043+SadiqIbrahim01@users.noreply.github.com>
2025-07-16 14:07:12 -0700

    Add files via upload

---
+++ Chapter Two/Multiples.py
@@ -0,0 +1,7 @@
+#Multiples
+print ('Determine whether 1024 is a mulple of 4 and whether 2 is a multiple of 10')
+
+if 1024 % 4 == 0:
+    print('1024 is a multiple of 4')
+if 10 % 2 == 0:
+    print('10 is a multiple of 2')
