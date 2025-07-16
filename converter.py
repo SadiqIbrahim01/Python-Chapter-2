e28f3aa590003cc4f330c32ebf10a4693ae6c2aa
SadiqIbrahim01 <164149043+SadiqIbrahim01@users.noreply.github.com>
2025-07-16 07:21:23 -0700

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,11 @@
+print ('This converts Miles to Kilometer')
+
+print ('Value of miles should be 175')
+
+miles = 175
+
+MILES_PER_KILOMETER = 1.609
+
+kilometers = miles * MILES_PER_KILOMETER
+
+print ('Value of 175 miles in kilometers is ', kilometers)
