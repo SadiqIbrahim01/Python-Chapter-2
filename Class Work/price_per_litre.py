972343a5b3e2d5656d474bdddc56ebdce60e8d12
SadiqIbrahim01 <164149043+SadiqIbrahim01@users.noreply.github.com>
2025-07-16 07:45:36 -0700

    Add files via upload

---
+++ Class Work/price_per_litre.py
@@ -0,0 +1,12 @@
+#Price per litre
+
+print ('Price per litre is #855')
+
+LITRES_OF_FUEL = 855
+
+budget = int (input('How much fuel would you like to purchase? '))
+
+litres = budget / LITRES_OF_FUEL
+
+print ('You can purchase ', litres, 'of fuel')
+
